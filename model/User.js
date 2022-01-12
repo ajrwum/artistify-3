48dc65d453fe2c8a8d91417114e8845c6e536863
Melodie Shahrvari <mshahrvari@gmail.com>
2022-01-12 17:16:07 +0100

    Begin route post signup

--- model/User.js
+++ model/User.js
@@ -1,26 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema 
+const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username : String,
-    email : {
-        type : String,
-        unique : true,
-    },
-    password : String,
-    role :{
-        type : String,
-        enum : ["admin", "user"],
-        default : "user",
-    },
-    avatar : {
-        type : String,
-        default : "https://res.cloudinary.com/gdaconcept/image/upload/v1614762472/workshop-artistify/default-profile_tbiwcc.jpg" 
-    }
-})
+  username: String,
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: String,
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  avatar: {
+    type: String,
+    default:
+      "https://res.cloudinary.com/gdaconcept/image/upload/v1614762472/workshop-artistify/default-profile_tbiwcc.jpg",
+  },
+});
 
-const User = model("user",UserSchema)
+const User = mongoose.model("user", UserSchema);
 
-module.exports = User
-
+module.exports = User;
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,32 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const fileUploader = require("./../config/cloudinary");
+const User = require("./../model/User.js");
+const bcrypt = require("bcrypt");
 
-router.get("/signin",(req, res, next) =>{
-    res.render("auth/signin")
-})
+// Routes
+router.get("/signin", (req, res, next) => {
+  res.render("auth/signin");
+});
 
 router.get("/signup", (req, res, next) => {
-    res.render("auth/signup")
-}) 
+  res.render("auth/signup");
+});
 
+router.post(
+  "/signup",
+  fileUploader.single("avatar"), //middleware to handle the decription of the req.body
+  async (req, res, next) => {
+    try {
+      console.log(req.body);
+      const avatar = req.file ? req.file.path : undefined;
+      const newUser = { ...req.body, avatar };
+      console.log(newUser);
+      await User.create(newUser);
+      res.redirect("/dashboard");
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-
-module.exports = router 
+module.exports = router;
